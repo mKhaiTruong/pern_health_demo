ae463194571b9a04d30818f0af04341b33c1dc85
mKhaiTruong <minhkhai0402@gmail.com>
2024-06-07 08:32:58 +0700

    vercel

--- server/db.js
+++ server/db.js
@@ -13,11 +13,7 @@ require("dotenv").config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    connectionString: process.env.POSTGRES_URL,
 });
 
 module.exports = pool;
